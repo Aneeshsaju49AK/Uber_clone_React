42989d06385bcb79b313345dc56bbd00ce7ab872
Aneesh Saju <aneeshsaju73@gmail.com>
2024-12-27 01:32:21 +0530

    create user login logout profile routes working fine

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,3 +1,4 @@
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
@@ -20,4 +21,35 @@ module.exports.registerUser = async (req, res, next) => {
 
     const token = user.generateAuthToken();
     res.status(201).json({success : true,token, user});
+}
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+    const user = await userModel.findOne({email}).select('+password');
+    if(!user){
+        return res.status(401).json({success: false, message: 'Invalid credentials'});
+    }
+    const isMatch = await user.comparePassword(password);
+    if(!isMatch){
+        return res.status(401).json({success: false, message: 'Invalid credentials'})
+    }
+    const token = user.generateAuthToken();
+    res.cookie('token',token);
+    res.status(200).json({success: true, token, user});
+}
+
+module.exports.getUserProfile = async (req, res, next) =>{
+    res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) =>{
+    res.clearCookie('token');
+    const token = req.cookies.token  || req.headers.authorization?.split(' ')[1]; 
+    await blacklistTokenModel.create({token});
+    res.status(200).json({success: true, message: 'User logged out successfully'});
 }
